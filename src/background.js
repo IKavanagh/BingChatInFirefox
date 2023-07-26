65fd071828a398d3a8a9ddbfef0f73e574e73108
Ian Kavanagh <ian.kavanagh@azyra.com>
2023-07-26 11:35:10 +0100

    Improve modifying request header
    Only modify request header for bing.com

--- src/background.js
+++ src/background.js
@@ -1,22 +1,21 @@
-const userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.100.0";
-const domains = ["bing.com", "msn.com", "live.com"];
+const DESKTOP_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48";
 
 browser.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
-        const url = details.url;
-        for (let i = 0; i < domains.length; i++) {
-            if (url.includes(domains[i])) {
-                for (let j = 0; j < details.requestHeaders.length; j++) {
-                    if (details.requestHeaders[j].name === "User-Agent") {
-                        details.requestHeaders[j].value = userAgentString;
-                        break;
-                    }
-                }
-                return { requestHeaders: details.requestHeaders };
+        if (!details.requestHeaders) {
+            return;
+        }
+
+        for (let i = 0; i < details.requestHeaders.length; i++) {
+            if (details.requestHeaders[i].name.toLowerCase() === "user-agent") { // headers are case-insensitive
+                details.requestHeaders[i].value = DESKTOP_USER_AGENT;
+                break;
             }
         }
+
+        return { requestHeaders: details.requestHeaders };
     },
-    { urls: ["<all_urls>"] },
+    { urls: ["*://*.bing.com/*"] },
     ["blocking", "requestHeaders"]
 );
 

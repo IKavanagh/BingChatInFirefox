54fc86ae6d29aae51dae022d4854c5b5843a8fde
Ian Kavanagh <ian.kavanagh@azyra.com>
2023-10-16 15:50:43 +0100

    Update User Agent string

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const DESKTOP_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48";
+const DESKTOP_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46";
 
 browser.webRequest.onBeforeSendHeaders.addListener(
     function (details) {

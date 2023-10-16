a4c55630f4df077bf10a64d07f332e9a58a2e616
Ian Kavanagh <ian.kavanagh@azyra.com>
2023-10-16 16:06:20 +0100

    Add support for mobile browsers

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 const DESKTOP_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46";
+const MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Mobile Safari/537.36 EdgA/117.0.2045.65"
 
 browser.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
@@ -8,7 +9,12 @@ browser.webRequest.onBeforeSendHeaders.addListener(
 
         for (let i = 0; i < details.requestHeaders.length; i++) {
             if (details.requestHeaders[i].name.toLowerCase() === "user-agent") { // headers are case-insensitive
-                details.requestHeaders[i].value = DESKTOP_USER_AGENT;
+                if (details.requestHeaders[i].value.toLowerCase().includes("mobile")) {
+                    details.requestHeaders[i].value = MOBILE_USER_AGENT;
+                }
+                else {
+                    details.requestHeaders[i].value = DESKTOP_USER_AGENT;
+                }
                 break;
             }
         }
